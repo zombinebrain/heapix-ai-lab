e5dc7a30313a4f064d8e31ffcc1ebad6c8116d8e
zombinebrain <an.kachynski@heapix.com>
2023-08-18 12:44:43 +0300

    Fix

--- src/components/icons/IconMinus.tsx
+++ src/components/icons/IconMinus.tsx
@@ -1,6 +1,10 @@
+type IconMinusProps = {
+  className?: string
+}
+
 const IconMinus = ({
-  className,
-}) => {
+  className = '',
+}: IconMinusProps) => {
   return (
     <svg className={`stroke-white ${className}`} width="100%" height="100%" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M22 12.5L3 12.5" strokeWidth="1.5" strokeLinecap="round"/>
