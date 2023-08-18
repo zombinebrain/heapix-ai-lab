dd960bb3c68c927ca1c24fbc7dc480827e0e7c53
zombinebrain <an.kachynski@heapix.com>
2023-08-18 13:11:41 +0300

    Fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: './'
+  output: "standalone",
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
