e191ec3d239261f6d67df093c88023dc451fc4a8
zombinebrain <an.kachynski@heapix.com>
2023-08-23 17:25:38 +0300

    Add onScroll text coloring

--- src/components/sections/AboutUsSection.tsx
+++ src/components/sections/AboutUsSection.tsx
@@ -16,7 +16,7 @@ import {
   useMotionValue,
   useVelocity,
   wrap,
-  useAnimationFrame
+  useAnimationFrame, Variants, useInView
 } from "framer-motion";
 import {useCallback, useEffect, useLayoutEffect, useRef, useState} from "react";
 
@@ -34,19 +34,24 @@ const AboutUsSection = () => {
     <IconPlatformKeras />
   ];
 
-
   const { scrollYProgress } = useScroll();
-  const moveX = useTransform(scrollYProgress, [0, 1], ["170%", "-170%"])
+
+  const moveX = useTransform(scrollYProgress, [0, 1], ["170%", "-170%"]);
 
   return (
     <>
-      <section id="about" className="overflow-hidden text-title base-padding base-vertical-grid gap-y-7.5 sm:flex sm:flex-col">
-        <p className="text-grey-400 col-span-4 tablet:col-span-3">
-          At  HEAPIX, we are AI experts specialising in large language and diffusion models.
-        </p>
-        <p className="row-start-2 col-start-5 col-span-full tablet:col-start-2">
-          We harness the power of OpenAI's GPT-3.5, GPT-4, LLaMa 2, Stable Diffusion, Claude, and Falcon, utilising Python, LangChain, HuggingFace, and Pinecone's vector databases.
-        </p>
+      <motion.section
+        id="about"
+        className="overflow-hidden text-title base-padding base-vertical-grid gap-y-7.5 sm:flex sm:flex-col"
+      >
+        <Paragraph
+          text="At  HEAPIX, we are AI experts specialising in large language and diffusion models."
+          className="col-span-4 tablet:col-span-3"
+        />
+        <Paragraph
+          text="We harness the power of OpenAI's GPT-3.5, GPT-4, LLaMa 2, Stable Diffusion, Claude, and Falcon, utilising Python, LangChain, HuggingFace, and Pinecone's vector databases."
+          className="row-start-2 col-start-5 col-span-full tablet:col-start-2"
+        />
         <motion.div
           style={{ x: moveX }}
           className="col-span-full flex space-x-5 sm:space-x-1.5"
@@ -59,13 +64,49 @@ const AboutUsSection = () => {
             ))
           }
         </motion.div>
-        <p className="text-grey-400 col-span-8 tablet:col-span-full">
-          We transform data into insights, optimising processes for smarter and informed decisions.
-        </p>
-      </section>
+        <Paragraph
+          text="We transform data into insights, optimising processes for smarter and informed decisions."
+          className="text-grey-400 col-span-8 tablet:col-span-full"
+        />
+      </motion.section>
       {/*<div ref={ghostRef} style={{ height: scrollRange }} className="ghost" />*/}
     </>
   );
 };
 
 export default AboutUsSection;
+
+type ParagraphProps = {
+  className: string,
+  text: string
+}
+
+const textVariants: Variants = {
+  offscreen: {
+    color: '#82858C'
+  },
+  onscreen: {
+    color: 'white',
+    transition: {
+      duration: 0.3
+    }
+  }
+};
+
+const Paragraph = ({ className = '', text = '' }: ParagraphProps) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false, amount: "all", margin: "-20% 0px -20% 0px" });
+
+  return (
+    <motion.p
+      ref={ref}
+      style={{
+        color: isInView ? 'white' : '#82858C'
+      }}
+      transition={{ duration: .3 }}
+      className={className}
+    >
+      { text }
+    </motion.p>
+  );
+};
